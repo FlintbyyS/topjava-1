8d02f3ca506932bff20226662244e4542ba91c92
FlintbyyS <muzikbaev2002@mail.ru>
2023-02-18 14:58:55 +0700

    add base repository

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -13,50 +13,20 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 @Repository
-public class InMemoryUserRepository implements UserRepository {
-    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
-
-    private final Map<Integer, User> repository = new ConcurrentHashMap<>();
-    private final AtomicInteger counter = new AtomicInteger(0);
-
-    public static final int USER_ID = 1;
-    public static final int ADMIN_ID = 2;
-
-    @Override
-    public boolean delete(int id) {
-        log.info("delete {}", id);
-        return repository.remove(id) != null;
-    }
-
-    @Override
-    public User save(User user) {
-        log.info("save {}", user);
-        if (user.isNew()){
-            user.setId(counter.incrementAndGet());
-            repository.put(user.getId(), user);
-            return user;
-        }
-        return repository.computeIfPresent(user.getId(), (id,oldUser) -> user);
-    }
-
-    @Override
-    public User get(int id) {
-        log.info("get {}", id);
-        return repository.get(id);
-    }
+public class InMemoryUserRepository extends InMemoryBaseRepository<User> implements UserRepository {
+    static final int USER_ID = 1;
+    static final int ADMIN_ID = 2;
 
     @Override
     public List<User> getAll() {
-        log.info("getAll");
-        return repository.values().stream()
+        return getCollection().stream()
                 .sorted(Comparator.comparing(User::getName).thenComparing(User::getEmail))
                 .collect(Collectors.toList());
     }
 
     @Override
     public User getByEmail(String email) {
-        log.info("getByEmail {}", email);
-        return repository.values().stream()
+        return getCollection().stream()
                 .filter(u -> email.equals(u.getEmail()))
                 .findFirst()
                 .orElse(null);
